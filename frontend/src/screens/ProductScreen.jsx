05bbed2f7b22920b53a0658acec031e763f6209d
grzeznx2 <grzeznx2@gmail.com>
2020-10-10 18:27:20 +0200

    Implement React Router

---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const ProductScreen = () => {
+  const { id } = useParams()
+  return <div>{`Product ${id}`}</div>
+}
+
+export default ProductScreen
