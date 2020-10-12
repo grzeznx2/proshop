3fe36391e8f6d2e00243d5e7ae99111cb3781e37
grzeznx2 <grzeznx2@gmail.com>
2020-10-12 12:44:27 +0200

    Add product details reducer $ actions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,6 +3,9 @@ import {
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
 } from '../constants/productConstants'
 
 export const listProducts = () => async dispatch => {
@@ -22,6 +25,28 @@ export const listProducts = () => async dispatch => {
       type: PRODUCT_LIST_FAIL,
       payload:
         //   jeżeli mamy własną wiadomość, to używamy jej, jeżeli nie, używamy wiadomości generycznej
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+export const listProductDetails = id => async dispatch => {
+  try {
+    dispatch({
+      type: PRODUCT_DETAILS_REQUEST,
+    })
+
+    const { data } = await axios.get(`/api/products/${id}`)
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
         error.response && error.response.data.message
           ? error.response.data.message
           : error.message,
